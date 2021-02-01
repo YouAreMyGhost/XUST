c4ae8492fb90302a935e59722d87584e2e22ea0b
YouAreMyGhost <75360552+YouAreMyGhost@users.noreply.github.com>
2021-02-01 01:34:22 +0000

    Add files via upload

---
+++ cppTest1.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>	
+using namespace std;
+
+void Func(int a = 11, int b = 22, int c = 33)
+{
+	cout << "a=" << a <<" " ;
+	cout << "b=" << b <<" " ;
+	cout << "c=" << c <<" " ;
+	cout << endl;
+}
+//int add(int a, int b)
+//{
+//	//cout<<a + b<<endl;
+//	return a + b;
+//}
+
+
+int add(int a, int b, int c)
+{
+	return a + b + c;
+}
+
+double add(double a, double b, double c)
+{
+	return a + b + c;
+}
+int main()
+{
+	cout << add(1, 2, 3) << endl;
+	cout << add(1.1, 2.2, 3.3) << endl;
+	return 0;
+}                                                                                         
